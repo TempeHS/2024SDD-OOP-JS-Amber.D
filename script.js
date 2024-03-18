6922bbd648a5bd72a2798ebe7b8b5dcaa1e874eb
Amber <136765021+Amberdong27@users.noreply.github.com>
2024-03-18 00:50:05 +0000

    Video Code

--- script.js
+++ script.js
@@ -46,12 +46,44 @@ const darthVader = createPlayer (
 console.log(darthVader); */
 
 
-class Player {}
-const hanSolo = new Player();
-console.log (hanSolo);
-console.long (getOwnPropertyNames(hanSolo));
-console. log (getOwnPropertyNames(hansolo._proto_) );
 
-function AnotherPlayer() {} 
-const darthVader = new AnotherPlayer();
-console.log (darthVader);
+class Player {
+    constructor(name, hp , mp, items) {
+        this.name = name;
+        this.hp = hp;
+        this.mp = mp;
+        this.items = items;
+    } 
+    speak(phrase) {
+    console.log(`${this.name} says: ${phrase}`);
+}
+}
+const hanSolo = new Player("Han Solo", 100, 10, ["Blaster"]);
+hanSolo.speak("Never tell me the odds");
+console.log (hanSolo);
+console.log (Object.getOwnPropertyNames(hanSolo));
+console.log (Object.getOwnPropertyNames(hanSolo._proto_));
+
+//const nader = new Player("Nader", 10, 5, ["Computer"]);
+//nader.speak("Monkeys are the best animal!");
+//console.log (nader);
+//console.log (Object.getOwnPropertyNames(nader));
+//console.log (Object.getOwnPropertyNames(nader._proto_));
+
+function AnotherPlayer(name, hp, mp, items) {
+    this.name = name;
+    this.hp = hp;
+    this.mp = mp;
+    this.items = items;
+} 
+
+AnotherPlayer.prototype.speak = function (phrase) {
+    console.log(`${this.name} says: ${phrase}`);
+
+}
+
+const darthVader = new AnotherPlayer("Darth Vader", 200, 50, ["Saber"]);
+console.log(darthVader);
+darthVader.speak("Hshfsfiihhhhhh");
+console.log (Object.getOwnPropertyNames(darthVader));
+console.log (Object.getOwnPropertyNames(darthVader._proto_));
