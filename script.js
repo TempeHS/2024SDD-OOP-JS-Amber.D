5bac4512a38c0b0e9e53c3ae804791fe714aa604
Amber <136765021+Amberdong27@users.noreply.github.com>
2024-03-22 02:27:53 +0000

    working on space invaders

--- script.js
+++ script.js
@@ -1,7 +1,59 @@
 class player { 
-    constructor (lives, score, position) {
+    constructor (lives, score, positionX, positionY, bullets) {
         this.lives = lives;
         this.score = score;
-        this.position = position;
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.bullets = bullets;
+    }
+    updateScore (newScore) {
+        this.score.push(newScore);
+    }
+    shoot () {
+    if(this.bullets == 0 ){
+        return ("out of bullets");
+    } else {
+        this.bullets = this.bullets - 1;
+        return(this.bullets);
+    }
+    }
+    remain () {
+        if (this.lives == 0) {
+            return("you died");
+        } else { 
+            this.lives = this.lives - 1;
+            return(this.lives);
+        }
     }
 }
+
+const p1 = new player(3, [], 5, 4, 10);
+console.log(p1);
+console.log(Object.getOwnPropertyNames(p1));
+p1.updateScore(300);
+
+
+
+class enemies {
+    constructor (number, positionX, positionY) {
+        this.number = number;
+        this.positionX = positionX; 
+        this.positionY = positionY;
+    }
+    hit(pX, pY) {
+        if ( this.positionX == pX && this.positionY == pY){ 
+            p1.shoot();
+            p1.remain();
+            return("hit");
+        } else {
+            p1.shoot();
+            return("miss");
+        }
+
+    }
+}
+
+const e1 = new enemies("e1", 5, 4);
+console.log(e1);
+console.log(Object.getOwnPropertyNames(e1)); 
+console.log(e1.hit(p1.positionX, p1.positionY));
