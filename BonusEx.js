4d51b0a3b328a91d3d3700d11cc6b819a4c03dc1
Amber <136765021+Amberdong27@users.noreply.github.com>
2024-03-25 05:28:33 +0000

    25-03-2024

---
+++ BonusEx.js
@@ -0,0 +1,12 @@
+const factory = new EnemyFactory(); 
+const flying = factory.generateFLyingEnemy("batman"); //method of factory 
+flying.fly(); //batman can fly!
+
+/* const swimming = EnemyFactory.generateSwimmingEnemy("aquaman");
+swimming.swim(); //aquaman can swim! */
+
+class EnemyFactory {
+generateFLyingEnemy (nam) {
+    const nam = new class }
+}
+
--- script.js
+++ script.js
@@ -27,7 +27,7 @@ class player {
     }
 }
 
-const p1 = new player(3, [], 5, 4, 10);
+const p1 = new player(3, [2], 5, 4, 10);
 console.log(p1);
 console.log(Object.getOwnPropertyNames(p1));
 p1.updateScore(300);
@@ -44,16 +44,45 @@ class enemies {
         if ( this.positionX == pX && this.positionY == pY){ 
             p1.shoot();
             p1.remain();
+            this.remainEnemy();
             return("hit");
         } else {
             p1.shoot();
             return("miss");
         }
-
+    }  remainEnemy() {
+        this.number = this.number - 1;
     }
 }
 
-const e1 = new enemies("e1", 5, 4);
+const e1 = new enemies(4, 5, 4);
 console.log(e1);
 console.log(Object.getOwnPropertyNames(e1)); 
 console.log(e1.hit(p1.positionX, p1.positionY));
+
+
+/* Sample Notes
+class Player {
+    lives =3; 
+    shootDelay = 2000; //ms
+    yPos = 500;
+    xPos = 500;
+}
+
+class Enemy {
+    hp = 1;
+    shootingDelay = 9000;
+    yPos;
+    xPos;
+    score = 10;
+
+    constructor(xPos, yPos) {
+        this.xPos = xPos;
+        this.yPos = yPos;
+    }
+}
+const spaceship = new Player();
+console.log(spaceship);
+const alien1 = new Enemy(10, 10);
+console.log(alien1);
+*/
